7fa589913078eebdee210199d02d3fcd24525711
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-31 18:58:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,12 +6,12 @@ def using_unshift(array, value)
   array.unshift(value)
 end
 
-def using_pop
-
+def using_pop(array)
+  arrays.pop
 end
 
-def pop_with_args
-
+def pop_with_args(array)
+array.pop(2)
 end
 
 def using_shift
