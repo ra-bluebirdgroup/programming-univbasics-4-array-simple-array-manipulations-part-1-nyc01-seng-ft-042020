5e07b25afa41f97d2739b3646f9fa19c58c755da
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-31 18:56:39 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(array, value)
+array.push(value)
+end
+
+def using_unshift
+
+end
+
+def using_pop
+
+end
+
+def pop_with_args
+
+end
+
+def using_shift
+
+end
+
+def shift_with_args
+
+end
