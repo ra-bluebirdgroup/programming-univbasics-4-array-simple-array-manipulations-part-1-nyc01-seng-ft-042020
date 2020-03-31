e94c7215893d8af47126ba23f2d66dc95777e39e
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-31 19:01:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,10 +15,11 @@ array.pop(2)
 end
 
 def using_shift(array)
- array.shift
-array
+  array.shift
+  array
 end
 
-def shift_with_args
-
+def shift_with_args(array)
+    values = array.shift(2)  
+    values    
 end
